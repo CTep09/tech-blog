cf0793a9240099e3a7f22783479bd22d5dc8ff3e
Cassie T <cassandray9@gmail.com>
2023-05-07 12:24:41 -0600

    home-routes.js updated, comment and newPost js complete

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,19 +1,58 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
 
-router.get('/', async (req, res) => { 
-    res.render('homepage')
-})
-router.get('/login', async (req, res) => { 
-    res.render('login')
-})
-router.get('/signup', async (req, res) => { 
-    res.render('signup')
-})
-router.get('/signup', async (req, res) => { 
-    res.render('dashboard')
-})
-router.get('/logout', async (req, res) => { 
-    res.render('logout')
-})
+// all posts
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render("all-posts", { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// single post
+router.get("/posts/id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render("single-post", { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+            res.redirect('/');
+            return;
+        }
+        res.render('login');
+    });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+            res.redirect('/');
+            return;
+        }
+        res.render('signup');
+    });
 
 module.exports = router;
